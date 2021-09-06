99fa13f7c1164adf7f98242e5df4ef0c284da68f
Shane McDonough <shanemcdman@gmail.com>
2021-09-06 14:43:07 -0700

    Allow ability to recurse

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,40 @@ struct RenameExtOpt{
     recursive: bool,
 }
 
-fn rename_ext(opt: RenameExtOpt) -> std::io::Result<()>{
-    let current_dir = env::current_dir()?;
+fn rename_ext_at(opt: &RenameExtOpt, path: PathBuf) -> std::io::Result<()>{
     let new_ext = &opt.new_ext
+        .clone()
         .unwrap_or(String::new());
     let new_ext = new_ext.as_str();
-    for entry in fs::read_dir(current_dir.clone())? {
-        let file_name = entry?.file_name();
-        let file_name = file_name.to_string_lossy();
-        if file_name.ends_with(&opt.old_ext){
-            let new_file_name = file_name.replace(
-                &opt.old_ext,
-                new_ext,
-                );
-            let mut old_path = current_dir.clone();
-            old_path.push(Path::new(&*file_name));
-            let mut new_path = current_dir.clone();
-            new_path.push(new_file_name);
-            fs::rename(old_path, new_path)?;
+    for entry in fs::read_dir(path.clone())? { // read directory
+        let entry = entry?;
+        if entry.path().is_dir() && opt.recursive { // if it is a directory
+            rename_ext_at(opt, entry.path())?; // recurse
+        } else {
+            let file_name = entry.file_name(); // get file name
+            let file_name = file_name.to_string_lossy(); // convert file name to Cow<String>
+            if file_name.ends_with(&opt.old_ext){ // if the file name ends with extension
+                let new_file_name = file_name.replace( // replace it
+                    &opt.old_ext,
+                    new_ext,
+                    );
+                let mut old_path = path.clone();
+                old_path.push(Path::new(&*file_name));
+                let mut new_path = path.clone();
+                new_path.push(new_file_name);
+                fs::rename(old_path, new_path)?; // rename the file
+            }
         }
     }
     Ok(())
 }
 
+fn rename_ext(opt: &RenameExtOpt) -> std::io::Result<()>{
+    let current_dir = env::current_dir()?;
+    rename_ext_at(opt, current_dir)
+}
+
 fn main() -> std::io::Result<()> {
     let opt = RenameExtOpt::from_args();
-    let opt = dbg!(opt);
-    rename_ext(opt)
+    rename_ext(&opt)
 }
