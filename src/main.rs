e191b5c510a7881b9cc1724c7aa78ed6698f8662
Shane McDonough <shanemcdman@gmail.com>
2021-09-07 14:43:47 -0700

    rename to rename_all and remove extension requirement

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,34 @@ use std::env;
 use std::fs;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "rename_ext", about = "Mass rename all files with an extension to another extension")]
-struct RenameExtOpt{
-    #[structopt(name = "OLD EXTENSION")]
-    old_ext: String,
+#[structopt(name = "rna", about = "Mass rename all files with one pattern to a different pattern")]
+struct RenameAllOpt{
+    #[structopt(name = "Pattern")]
+    pattern: String,
 
-    #[structopt(name = "NEW EXTENSION")]
-    new_ext: Option<String>,
+    #[structopt(name = "Replacement")]
+    replacement: Option<String>,
 
     #[structopt(short, long)]
     recursive: bool,
 }
 
-fn rename_ext_at(opt: &RenameExtOpt, path: PathBuf) -> std::io::Result<()>{
-    let new_ext = &opt.new_ext
+fn rename_all_at(opt: &RenameAllOpt, path: PathBuf) -> std::io::Result<()>{
+    let replacement = &opt.replacement
         .clone()
         .unwrap_or(String::new());
-    let new_ext = new_ext.as_str();
+    let replacement = replacement.as_str();
     for entry in fs::read_dir(path.clone())? { // read directory
         let entry = entry?;
         if entry.path().is_dir() && opt.recursive { // if it is a directory
-            rename_ext_at(opt, entry.path())?; // recurse
+            rename_all_at(opt, entry.path())?; // recurse
         } else {
             let file_name = entry.file_name(); // get file name
             let file_name = file_name.to_string_lossy(); // convert file name to Cow<String>
-            if file_name.ends_with(&opt.old_ext){ // if the file name ends with extension
+            if file_name.contains(&opt.pattern){ // if the file name has the pattern
                 let new_file_name = file_name.replace( // replace it
-                    &opt.old_ext,
-                    new_ext,
+                    &opt.pattern,
+                    replacement,
                     );
                 let mut old_path = path.clone();
                 old_path.push(Path::new(&*file_name));
@@ -47,12 +47,12 @@ fn rename_ext_at(opt: &RenameExtOpt, path: PathBuf) -> std::io::Result<()>{
     Ok(())
 }
 
-fn rename_ext(opt: &RenameExtOpt) -> std::io::Result<()>{
+fn rename_all(opt: &RenameAllOpt) -> std::io::Result<()>{
     let current_dir = env::current_dir()?;
-    rename_ext_at(opt, current_dir)
+    rename_all_at(opt, current_dir)
 }
 
 fn main() -> std::io::Result<()> {
-    let opt = RenameExtOpt::from_args();
-    rename_ext(&opt)
+    let opt = RenameAllOpt::from_args();
+    rename_all(&opt)
 }
